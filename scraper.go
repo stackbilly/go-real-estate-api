2043934de7a41447c95e0d300b9df86dfd347955
Livingstone-Billy <livingstoneochadia@gmail.com>
2023-10-19 10:57:32 +0300

    write scrape data straight to DB

--- scraper.go
+++ scraper.go
@@ -97,7 +97,7 @@ func WriteToCSV(filename string, houses []House) error {
 }
 
 func Scrape(url string, filename string, limit int) (colly.Collector, error) {
-	//scraper config
+	//scraper configurations
 	c := colly.NewCollector(colly.Async(true))
 
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
